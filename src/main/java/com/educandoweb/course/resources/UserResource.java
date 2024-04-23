d0538f6859d0763d295b55bd002c0a74d41ded24
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-23 10:24:17 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,25 +1,32 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController // Indicador ao framework
+// indica que se trata de um controlador Rest, voltado para o desenv. de apliações web Restful
+@RequestMapping(value = "/users") //Anotação de nível de classe
+//Define o prefixo de URL para todas as rotas de um controller.
 public class UserResource {
 
-	@Autowired
+	@Autowired // Cria dependências e acoplar dif. camadas da aplicação
+	// Controle sobre como deve ser realizada a lig. entre os beans
 	private UserService service;
 
-	@GetMapping
+	@GetMapping // Define uma rota que responde a req. HTTP GET
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
@@ -29,5 +36,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping // Define que esse método vai receber o método 'Post' do HTTP
+	// Faz um pré processamento na compilação do compilador
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
